014cd69495db0eba635bf2dc63d488a90b8b6a6d
akkiirah <akkiirah@gmail.com>
2021-12-12 12:45:06 +0100

    first commit

---
+++ src/main/java/net/akibc/betterconcrete/BetterConcreteMod.java
@@ -0,0 +1,10 @@
+package net.akibc.betterconcrete;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterConcreteMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
